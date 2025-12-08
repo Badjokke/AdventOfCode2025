bb9d146cab096429c9b84cbf8331156f9a309c89
badjokke <badjokke@gmail.com>
2025-12-08 23:31:53 +0100

    day6 part 2

--- six/src/main.rs
+++ six/src/main.rs
@@ -19,23 +19,49 @@ fn str_to_operator(str: &str) -> Operator {
     }
 }
 
+fn collect_number_from_column(number_index: usize, lines: &Vec<String>) -> u128 {
+    let mut result = 0_u128;
+    let n = lines.len() - 1;
+    let mut exp_walker = n;
+    let mut i = n;
+    while i != 0 {
+        i -= 1;
+        let mut chars = lines[i].chars();
+        let char = chars.nth(number_index).unwrap();
+        if char.is_whitespace() {
+            continue;
+        }
+        let digit = char.to_digit(10).unwrap() as u128;
+        result += digit * 10_u128.pow((n - exp_walker) as u32);
+        exp_walker -= 1;
+    }
+    result
+}
+
 fn collect_numbers_from_vertical_list(path: &str) -> Vec<Operation> {
     let lines = read_file_to_lines(path);
     let mut result: Vec<Operation> = vec![];
     let row_count = lines.len();
-    for i in 0..row_count - 1 {
-        lines[i].split_whitespace().enumerate().for_each(|(i, x)| {
-            if i == result.len() {
-                result.push(Operation(vec![], Operator::NONE));
-            }
-            result[i].0.push(x.trim().parse::<u128>().unwrap());
-        });
+    let mut walker = lines[row_count - 2].len();
+    let mut result_pointer = 0;
+    while walker != 0 {
+        walker -= 1;
+        if result_pointer == result.len() {
+            result.push(Operation(vec![], Operator::NONE));
+        }
+        let number = collect_number_from_column(walker, &lines);
+        if number == 0 {
+            result_pointer += 1;
+            continue;
+        }
+        result[result_pointer].0.push(number);
     }
+
     lines[row_count - 1]
         .split_whitespace()
         .map(|op| op.trim())
         .enumerate()
-        .for_each(|(i, op)| result[i].1 = str_to_operator(op));
+        .for_each(|(i, op)| result[result_pointer - i].1 = str_to_operator(op));
     result
 }
 
